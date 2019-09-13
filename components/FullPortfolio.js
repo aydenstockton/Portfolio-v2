c32aa2db711f6e5e853e8ca093cd1180a16a20e8
Stockton <gqw4sto@msl130657.ent.rt.csaa.com>
2019-09-12 17:30:54 -0700

    Thumbnail function abstraction and styling

--- components/FullPortfolio.js
+++ components/FullPortfolio.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import ProjectAPI from '../Api'
-import { Link } from 'react-router-dom'
 import Thumbnail from './Thumbnail'
+
+
+function Thumbnails(props) {
+    // const thumbs = ProjectAPI.all();
+    const thumbItems = props.thumbs.map((thumb) =>
+        <Thumbnail name={thumb.name} src={thumb.src} number={thumb.number} />
+    );
+    return (
+        <div className="work-wrapper">{thumbItems}</div>
+    );
+}
 
 // The FullPortfolio iterates over all of the projects and creates
 // a link to their project page.
 const FullPortfolio = () => (
-  <div className="work-wrapper">
-      <Thumbnail/>
-  </div>
+    <Thumbnails thumbs={ProjectAPI.all()}/>
 )
 
 export default FullPortfolio
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import ProjectAPI from "../Api";
 import {Link} from "react-router-dom";
-import ImageView from "./ImageView";
 
 
-const Thumbnail = ({name, src}) => (
-    ProjectAPI.all().map(p => (
-        <Link to={`/work/${p.number}`}>
-            <div className="Thumbnail" key={p.number}>
+const Thumbnail = ({name, src, number}) => (
+        <Link to={`/work/${number}`}>
+            <div className="Thumbnail" key={number}>
                 <div className="picture-wrapper">
-                    <img src={p.src}/>
+                    <img src={src}/>
                 </div>
                 <div className="name-wrapper">
-                    <p>{p.name}</p>
+                    <p>{name}</p>
                 </div>
             </div>
         </Link>
-     )
-));
+);
 
 export default Thumbnail
