4f67cb14b02be3a6f7c52421789192d7bccd4f1f
aydenstockton <ayden.stockton@gmail.com>
2019-09-16 08:05:17 -0700

    React router scroll to top

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-
+import ScrollToTop from 'react-router-scroll-top';
 import "./styles.pcss";
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
   </BrowserRouter>
 ), document.getElementById('root'));
