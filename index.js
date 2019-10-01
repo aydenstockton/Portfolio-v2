7f88110dcf63a81facce6987d05ae6b0bbd52a0f
aydenstockton <ayden.stockton@gmail.com>
2019-09-30 23:13:07 -0700

    Installed parallax

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import ScrollToTop from 'react-router-scroll-top';
 import "./styles.pcss";
+import {ParallaxProvider} from "react-scroll-parallax/cjs";
 
 ReactDOM.render((
-  <BrowserRouter>
-      <ScrollToTop>
-        <App />
-      </ScrollToTop>
-  </BrowserRouter>
+    <ParallaxProvider>
+      <BrowserRouter>
+          <ScrollToTop>
+            <App />
+          </ScrollToTop>
+      </BrowserRouter>
+    </ParallaxProvider>
 ), document.getElementById('root'));
