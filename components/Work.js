9863f989cc8b0b835a96c333d53fa2bfe24b5c31
aydenstockton <ayden.stockton@gmail.com>
2020-05-18 11:48:07 -0700

    Fixed thumbnail reorg anumation

--- components/Work.js
+++ components/Work.js
@@ -1,115 +1,57 @@
-import React from 'react'
-import ProjectAPI from '../ProjectAPI'
-import Thumbnail from './Thumbnail/Thumbnail'
-import posed, { PoseGroup }from "react-pose";
+import _ from "lodash";
+import React from "react";
+import ProjectAPI from "../ProjectAPI";
+import Thumbnail from "./Thumbnail/Thumbnail";
+import posed, { PoseGroup } from "react-pose";
 import ProjectFilters from "./ProjectFilters/ProjectFilters";
 
-const ContainerParent = function(){
- return (
-     <div></div>
- )   // enter: { staggerChildren: 50 }
+const Item = posed.div({
+  enter: { y: 0, x: 0, opacity: 1 },
+  exit: { y: -50, x: 0, opacity: 0 },
+  flip: {
+    scale: 1,
+    transition: {
+    //   scale: {
+    //     type: "spring",
+    //     velocity: 10
+    //   },
+      default: {
+        type: "spring",
+        stiffness: 200,
+        damping: 20,
+      }
+    }
+  }
+});
+
+export default ({}) => {
+  const [type, setType] = React.useState("all");
+  const [items, setItems] = React.useState(ProjectAPI.byProperty({ type }));
+  return (
+    <div type={type}>
+      <ProjectFilters
+        handleType={newType => {
+          setType(newType);
+          setItems(ProjectAPI.byProperty({ type: newType }));
+        }}
+      />
+      <div className="work-wrapper">
+        <PoseGroup
+          animateOnMount={false} enterPose='enter' exitPose='exit'
+        >
+          {items.map(thumb => (
+            <Item key={thumb.number}>
+              <Thumbnail
+                name={thumb.name}
+                src={thumb.src}
+                number={thumb.number}
+                id={thumb.id}
+              />
+            </Item>
+          ))}
+        </PoseGroup>
+      </div>
+    </div>
+  );
 };
 
-function Thumbnails(props) {
-    const Item = posed.div({
-        enter: { y: 50, x:0, opacity: 1, animateOnMount: true},
-        exit: { y: 0, x:0, opacity: 0 }
-        // flip: {
-        //     scale: 1,
-        //     transition: {
-        //         scale: {
-        //             type: 'spring',
-        //             velocity: 10
-        //         },
-        //         default: {
-        //             type: 'spring'
-        //         }
-        //     }
-        // }
-    })
-    const thumbItems = props.projects.map((thumb) =>
-        <Item key={thumb.number}>
-            <Thumbnail name={thumb.name} src={thumb.src} number={thumb.number} id={thumb.id}/>
-        </Item>
-            );
-    return (
-        <div className="work-wrapper">
-            <PoseGroup animateOnMount={false} flipMove={false} enterPose='enter' exitPose='exit'>{thumbItems}</PoseGroup>
-        </div>
-    );
-}
-// The Work iterates over all of the projects and creates
-// a link to their project page.
-class Work extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            type:null,
-            projects:[]
-        }
-        this.handleType = this.handleType.bind(this);
-    }
-    shouldComponentUpdate(nextProps, nextState) {
-        if(nextState.type===this.state.type) {
-            return false;
-        }
-        return true;
-    }
-    handleType(type, e) {
-        this.setState({
-            type:type,
-            projects:ProjectAPI.byProperty({type:type})
-        });
-    }
-
-    componentDidMount() {
-
-        // setInterval(() => {
-        //     var a = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
-        //     this.setState({
-        //         projects: ProjectAPI.all()[2]
-        //     });
-        // }, 1000);
-
-        // let state = this.props.location.state;
-        // if(!state) {
-        //     return false;
-        // }
-        //
-        // let type = this.props.location.state.type;
-        // if(type) {
-        //     this.setState({
-        //         type:type
-        //     });
-        // }
-        // this.state = {
-        //     type:this.props.location.state.type,
-        //     projects:ProjectAPI.all()
-        // }
-
-        let type = "all";
-        if(this.props.location.state && this.props.location.state.type) {
-            type=this.props.location.state.type;
-        }
-        this.handleType(type)
-    }
-    componentDidUpdate() {
-
-    }
-
-
-    render() {
-        document.getElementsByTagName("BODY")[0].setAttribute("theme", "universe");
-        document.getElementsByTagName("BODY")[0].setAttribute("page", "work");
-
-        return (
-            <div type={this.state.type}>
-                <ProjectFilters handleType={this.handleType}/>
-                <Thumbnails projects={this.state.projects} />
-            </div>
-        )
-    }
-}
-
-export default Work
